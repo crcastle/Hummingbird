2532c673d15ecb8a762ef956937ba9136bfa9360
Chris Castle <crcastle@gmail.com>
2011-01-05 17:11:58 -0800

    updated alert box text when socket goes down.

--- public/js/aggregator_client.js
+++ public/js/aggregator_client.js
@@ -52,7 +52,7 @@ $(function() {
 		if (!chartOptions.measure instanceof String) return;
 		if (!chartOptions.aggregation instanceof String) return;
 
-		if (! socket.connected) { alert("Socket disconnected. Refresh page."); return; }
+		if (! socket.connected) { alert("Socket to server disconnected. Refresh page."); return; }
 		if (chart) chart.showLoading();
 		sendRequest($.toJSON(chartOptions));
 	});
